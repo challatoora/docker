2ac203e8cb0fce98999e6f5d65235b51007ba084
challatoor murali <challatoor murali.gmail.com>
2026-01-31 20:10:31 +0530

    sucess

--- installing.sh
+++ installing.sh
@@ -8,4 +8,11 @@ if [ $user_id -ne 0 ]; then
 fi
 
 echo " installing nginix"
-dnf install nginx -y
+dnf install nginxx -y
+
+if [ $? -ne 0 ]; then
+    echo "intsalling nginix is failed"
+    exit 1
+else
+    echo "intsalling nginix is done"
+fi
