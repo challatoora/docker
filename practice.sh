f8ae2e718802d1ba09a5378e6345a04ad6134fa9
challatoor murali <challatoor murali.gmail.com>
2026-01-31 19:23:35 +0530

    sucess

---
+++ practice.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+name=ashok
+echo " my name is $name "
+
+N1=$1
+N2=$2
+
+sum=$(($N1+$N2))
+
+echo "sum is $sum"
+
+echo "Please enter a name"
+read ashok
+
+echo "my name is $ashok"
+
+total=$((2+5))
+echo $total
+
+
+echo "$@"
+echo "$#"
+echo "$PWD"
+echo "$?"
+
+
+
+
+
+
