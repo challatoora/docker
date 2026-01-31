82afa79745eb0db8d10d5f460b344d445ce56d6e
challatoor murali <challatoor murali.gmail.com>
2026-01-31 17:19:30 +0530

    sucess

--- message.sh
+++ message.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
+a=$(date +%s)
+sleep 10
+b=$(date+%s)
 
-echo " enter u r  firstm name: "
-read name 
+c=$((b-c))
 
-echo " enter u r  last  name: "
-read name1
-
-echo " enter u r  password: "
-read -s name2 
-
-echo "my name is $name and $name1"
-
-echo " $name2"
+echo "total time savings $c"
 
 
 
