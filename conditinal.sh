a9908d34efdbbcf97fead4e7236cd6864071983f
challatoor murali <challatoor murali.gmail.com>
2026-01-31 19:56:06 +0530

    sucess

--- conditinal.sh
+++ conditinal.sh
@@ -2,10 +2,10 @@
 
 num=$1
 
-if [ $num -gt 10 ]; then
-    echo " $num is a big num "
-elif [ $num -lt 10 ]; then
+if [ $num -gt 10 ] && [ $num -lt 10 ]; then
     echo " $num is a small num "
+elif [ $num -gt 10 ]; then
+    echo " $num is a big num "
 else
     echo " $num is a negative num "
 fi
