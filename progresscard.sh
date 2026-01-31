50c04a2df6d0a5ee946ff1b5c794d44d29d7bba5
challatoor murali <challatoor murali.gmail.com>
2026-01-31 22:13:39 +0530

    sucess

--- progresscard.sh
+++ progresscard.sh
@@ -32,22 +32,4 @@ marks=$1
 
 if [ "$marks" -lt 0 ] || [ "$marks" -gt 100 ]; then
     echo "Invalid marks"
-
-elif [ "$marks" -eq 35 ]; then
-    echo "Just pass"
-
-elif [ "$marks" -ge 36 ] && [ "$marks" -le 50 ]; then
-    echo "Second class"
-
-elif [ "$marks" -ge 51 ] && [ "$marks" -le 75 ]; then
-    echo "School first"
-
-elif [ "$marks" -ge 76 ] && [ "$marks" -le 90 ]; then
-    echo "State first"
-
-elif [ "$marks" -ge 91 ] && [ "$marks" -le 100 ]; then
-    echo "India first"
-
-else
-    echo "All the best"
 fi
