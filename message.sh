a4c6e3f2c73148a02805a99c14d2315be5d6e14a
challatoor murali <challatoor murali.gmail.com>
2026-01-31 16:48:32 +0530

    sucess

--- message.sh
+++ message.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo " enter u r name "
+echo " enter u r  firstm name: "
 read name 
 
-echo "my name is $name"
+echo " enter u r  last  name: "
+read name1
+
+
+echo "my name is $name and $name1"
 
 
 
