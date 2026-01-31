7172af9e319498b3376591fbccd486813d3f52a0
challatoor murali <challatoor murali.gmail.com>
2026-01-31 22:27:57 +0530

    sucess

--- progresscard.sh
+++ progresscard.sh
@@ -33,6 +33,8 @@ if [ "$marks" -lt 0 ] || [ "$marks" -gt 100 ]; then
     echo " invalid marks "
 elif [ $marks -eq 35 ]; then 
     echo " just pass"
+elif [ $marks -gt 35 ] && [ $marks -le 60 ]; then 
+    echo " second class"
 else
     echo "Good"
 fi
