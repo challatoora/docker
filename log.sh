c79e97e8c0a2995e832f250b48951b2f0d788f78
challatoor murali <challatoor murali.gmail.com>
2026-02-01 19:42:04 +0530

    sucess

---
+++ log.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+logfiles=/var/log/shell-script/$0.log
+
+customerid=$(id -u)
+
+if [ $customerid -eq 0 ]; then
+    echo " installaling my sql "
+else
+    echo " u r not root user"
+
+    exit 1
+fi
+
+validate(){ 
+if [ $1 -eq 0 ]; then
+    echo "$2 sucees"
+else
+    echo "$2 failed"
+fi
+}
+
+
+dnf install mysql -y &>> logfiles
+
+validate $? "mysql"
+
+dnf install nginx -y &>> logfiles
+
+validate $? " nginix" 
+
+
+
+
+
+  
+
