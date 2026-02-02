6a665b5737ac9b4a188e8470e6ae167b9932d4bd
challatoor murali <challatoor murali.gmail.com>
2026-02-02 19:47:05 +0530

    sucess

---
+++ reddy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+Logs_folder="/var/log/shell-script"
+logs_file="$logs_folder/$0.log"
+
+if [$USERID -ne 0 ]; then
+        echo " please run this script with root user " | tee -a $log_file
+        exit 1
+fi
+
+echo "Hi"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? " copiying Mongo Repo"
+
+dnf install mongodb-org -y 
+validate $? " installing mongoDB server"
+
+systemctl enable mongod 
+validate $? " enabled mongodb"
+
+systemctl start mongod 
+validate $? " start mongodb"
+
+sed -i 's/127.0.0.1 to 0.0.0.0/g' /etc/monogod.conf
+validate $? " allowing remote connection "
