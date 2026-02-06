bebfd7cce72aa15bfdf5d77d430686186f00e2f2
challatoor murali <challatoor murali.gmail.com>
2026-02-06 12:41:08 +0530

    sucess

--- Count_word.sh
+++ Count_word.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "my name is murali"
+# echo "my name is murali"
 
-words=$"my name is murali"
+# words=$"my name is murali"
  
- echo $words | wc -w
+#  echo $words | wc -w
+
+
+word=$1
+
+count=$word | wc -w
+echo $count
