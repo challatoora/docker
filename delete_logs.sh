582af89dad7497ccd4da5ea5593e4c4d1120c5ef
challatoor murali <challatoor murali.gmail.com>
2026-02-08 15:58:19 +0530

    sucess

--- delete_logs.sh
+++ delete_logs.sh
@@ -1,1 +1,17 @@
-hfsdb
+#!/bin/bash
+
+log_folder=/home/ec2-user/app-logs
+log_file="$log_folder/$0.log"
+
+
+
+if [ ! -d $log_folder ]; then
+    echo -e " The $log_folder is not availbe"
+    exit 1
+fi
+
+filesto_delete=$(find $log_folder -name "*.log" -type f  -mtime +14)
+echo "$filesto_delete"
+
+
+
