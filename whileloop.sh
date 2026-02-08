c9e7bccf1391047f6fe9d9286a586fbc3b6e34e0
challatoor murali <challatoor murali.gmail.com>
2026-02-08 16:09:49 +0530

    sucess

--- whileloop.sh
+++ whileloop.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-count=1
+# count=1
 
-while [ $count -le 5 ]
-do
-    echo "count is $count"
-    ((count++))
-done
+# while [ $count -le 5 ]
+# do
+#     echo "count is $count"
+#     ((count++))
+# done
+
+
+while IFS= read -r line; do
+    echo "$line"
+done < ./condition.sh # input which file to read
