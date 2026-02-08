951cb27e71b1e93e42827a46ff86e03ce911b714
challatoor murali <challatoor murali.gmail.com>
2026-02-08 18:01:05 +0530

    sucess

--- Backup.sh
+++ Backup.sh
@@ -6,6 +6,7 @@ Logs_file="$Logs_folder/Backup.log"
 source_dir=$1
 destination_dir=$2
 days=${3:-14}
+
 if [ $user_id -ne 0 ]; then
     echo " this is not a root user, hence run with sudo user"
     exit 1
@@ -16,6 +17,10 @@ mkdir -p $Logs_folder
 usage(){
     echo -e "sudo  backup <source> and <destination> and <Days> default is 14 days"
     exit
+}
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a Logs_file
 }
 
 if [ $# -lt 3 ]; then
@@ -29,6 +34,12 @@ if [ ! -d $source_dir ]; then
 fi
 
 if [ ! -d $destination_dir ]; then
-    echo " source directy is not there"
+    echo " directory directy is not there"
     exit 1
 fi
+
+files=$(find $source_dir -name "*.log" -type f -mtime +$days)
+
+log " backup started" 
+log " souce directory : $source_dir "
+log " destination directory : $destination_dir
