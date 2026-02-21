06cbe60dc8050167c007530d5c90c3b773972c19
challatoor murali <challatoor murali.gmail.com>
2026-02-21 10:43:32 +0530

    sucess

--- 05.sh
+++ 05.sh
@@ -1,8 +1,14 @@
 NUM1=100
-NUM2=murali
+NUM2=500
 
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is: $SUM"
 
 # Array
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
