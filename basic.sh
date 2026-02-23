f5c076d74f4b3fc172c8f40900b103fe42870bdd
challatoor murali <challatoor murali.gmail.com>
2026-02-23 18:43:15 +0530

    sucess

--- basic.sh
+++ basic.sh
@@ -2,27 +2,33 @@
 
 # Name1=murali
 # Name2=ashok
-
 # echo "$Name1 and $Name2"S
-
-
 # Name3=$1
 # Name4=$2
-
 # echo "$1 and $2"
-
-
-echo "please enter first name "
-read name 
-
-echo "my name is $name"
-
-echo " please enter password"
-read -s passwd
-
-echo "password is $passwd "
-
+# echo "please enter first name "
+# read name 
+# echo "my name is $name"
+# echo " please enter password"
+# read -s passwd
+# echo "password is $passwd "
 # folder1=/var/log/shell/
+
+
+N1=$1
+N2=$2
+N3=$3
+
+
+echo " total variables $@ "
+
+echo " name of the script is $0 "
+
+echo " exirt status $s "
+
+echo " present location $PWD "
+
+
 
 
 
