eed19e7f6e6e02c51fadcb2d2e6342fb76f51182
challatoor murali <challatoor murali.gmail.com>
2026-02-24 13:09:28 +0530

    sucess

--- loopspra.sh
+++ loopspra.sh
@@ -1,9 +1,16 @@
-for i in {1..20}
-do
+# for i in {1..20}
+# do
 
- if [ $((i % 2)) -eq 0 ]; then
-  echo " print $i this are the even number"
- else
-  echo " odd number "
- fi  
-done
+#  if [ $((i % 2)) -eq 0 ]; then
+#   echo " print $i this are the even number"
+#  else
+#   echo " odd number "
+#  fi  
+# done
+
+
+
+Num=$1
+if [ (($1+25)) ];then
+
+ echo " $1 values"
