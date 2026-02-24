6904b2300728b634d72068a5abe4fd471452317b
challatoor murali <challatoor murali.gmail.com>
2026-02-24 21:46:14 +0530

    sucess

--- errorhandling.sh
+++ errorhandling.sh
@@ -6,15 +6,15 @@ folder=/var/log/shell
 logs=$folder/$0.log
 
 mkdir -p  $folder
-if [ $user-ne 0 ]; then
+if [ $user -ne 0 ]; then
  echo " u r not user $user"
 fi
 
 validate(){
     if [ $1 -eq 0 ]; then
-        echo " $2 installing $2"
+        echo " $2 installing $2" | tee -a $logs
     else
-        echo " $2 failed "S
+        echo " $2 failed " | tee -a $logs
   fi
 }
 
