08d76e046bd38962ffeb861e62d114e3ad8f00b2
challatoor murali <challatoor murali.gmail.com>
2026-02-24 21:28:25 +0530

    sucess

---
+++ errorhandling.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+user=$(id -u)
+
+folder=/var/log/shell
+logs=$folder/$0.log
+
+mkdir -p  $folder
+if [ $user-ne 0 ]; then
+ echo " u r not user $user"
+fi
+
+validate(){
+    if [ $1 -eq 0 ]; then
+        echo " $2 installing $2"
+    else
+        echo " $2 failed "S
+  fi
+}
+
+dnf intall mysql -y 
+validate $? "mysql"
